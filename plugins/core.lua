33d16070f9d004b4017170738894648c4e7ea373
fffzlfk <1319933925qq@gmail.com>
2023-07-13 13:36:20 +0800

    feat(neo-tree): show hidden files

--- plugins/core.lua
+++ plugins/core.lua
@@ -74,4 +74,23 @@ return {
   --     }, { mode = "n", prefix = "<leader>" })
   --   end,
   -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+          },
+        },
+      },
+    },
+  },
 }
